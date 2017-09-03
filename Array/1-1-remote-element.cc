04dcfc237a95d617bca6a894fbece1f3a76889ee
lightfish <coolyeah@foxmail.com>
2017-09-04 00:21:13 +0800

    update

--- Array/1-1-remote-element.cc
+++ Array/1-1-remote-element.cc
@@ -1,30 +1,33 @@
-class Solution {
-public:
-    int removeElement(int A[], int n, int elem) {
-        int i = 0;
-        int j = 0;
-        for(i = 0; i < n; i++) {
-            if(A[i] == elem) {
-                continue;
-            }
-
-            A[j] = A[i];
-            j++;
-        }
-
-        return j;
-    }
+template <typename T>
+class Solution{
+    public:
+        int removeElement(T * A, int n, T elem);
 };
+
+template <typename T>
+int Solution<T>::removeElement(T * A, int n, T elem){
+    int j=0;
+    for(int i=0; i<n; i++){
+        if(elem != A[i]){
+            A[j++] = A[i];
+        }
+    }
+    return j;
+}
 
 #ifdef DEBUG
 #include <iostream>
 int
 main()
 {
-    Solution s;
+    Solution<int> s;
+    int n = 5;
     int a[] = {0, 1, 2, 3, 4};
-    std::cout << a[2] << std::endl;     
-    s.removeElement(a, 5, 2);
-    std::cout << a[2] << std::endl; 
+    for(int i=0; i<n; i++)  
+        std::cout << a[i] << std::endl;
+    n =s.removeElement(a, n, 2);
+    std::cout << "before:"<<std::endl;
+    for(int i=0; i<n; i++)  
+        std::cout << a[i] << std::endl;
 }
 #endif
--- Array/1-2-remove-duplicates-from-sorted-array.cc
+++ Array/1-2-remove-duplicates-from-sorted-array.cc
@@ -1,19 +1,21 @@
-class Solution {
-public:
-    int removeDuplicates(int A[], int n) {
-        if(n == 0) {
-            return 0;
-        }
-
-        int j = 0;
-        for(int i = 1; i < n; i++) {
-            if(A[j] != A[i]) {
-                A[++j] = A[i];
-            }
-        }
-        return j + 1;
-    }
+template <typename T>
+class Solution{
+    public:
+        int removeDuplicates(T * A, int n);
 };
+
+template <typename T>
+int Solution<T>::removeDuplicates(T * A, int n){
+    if(n <= 0)
+        return n;
+    int j=0;
+    for(int i=1; i<n; i++){
+        if(A[i] != A[j]){
+            A[++j] = A[i];
+        }
+    }
+    return j+1;
+}
 
 
 #ifdef DEBUG
@@ -21,12 +23,13 @@ public:
 int
 main()
 {
-    Solution s;
+    Solution<int> s;
     int n = 5;
     int a[5] = {0, 2, 2, 2, 4};
     for(int i=0; i<n; i++)  
         std::cout << a[i] << std::endl;
     n =s.removeDuplicates(a, n);
+    std::cout << "before:"<<std::endl;
     for(int i=0; i<n; i++)  
         std::cout << a[i] << std::endl;
 }
